5d9b132b2ecfb4102754c48f7a75f57319c57317
atxrenegade <harleighabel@gmail.com>
2021-01-19 16:20:44 -0600

    add new, create and show methods to Wrappers Controller

--- app/controllers/wrappers_controller.rb
+++ app/controllers/wrappers_controller.rb
@@ -1,2 +1,13 @@
 class WrappersController < ApplicationController
+  def new 
+    @wrapped_word = Wrapper.new
+  end 
+  
+  def create 
+    @wrapped_word = Wrapper.wrap(params[:col_num].to_i, params[:user_str])
+    render :show
+  end 
+  
+  def show
+  end 
 end
