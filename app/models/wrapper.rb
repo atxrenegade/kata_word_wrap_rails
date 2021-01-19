af1f030c40d702800baea767d540ff208d78cf28
atxrenegade <harleighabel@gmail.com>
2021-01-19 16:18:57 -0600

    add wrap method to wrapper class, remove active record inheritance

--- app/models/wrapper.rb
+++ app/models/wrapper.rb
@@ -1,3 +1,28 @@
-class Wrapper < ApplicationRecord
+class Wrapper 
+  #TO DO fix this algorithm
+  # condense head a tail to one block
 
-end
+  def self.wrap(col_num, user_str) 
+      user_str_arr = user_str.split()
+      new_str = ""
+      col_count = 0
+
+      user_str_arr.each_with_index do |word, index|
+          word_len = word.length
+          if ((word_len + index) < col_num) 
+              new_str += "#{word}"
+              col_count += (wordLen + 1)
+          elsif (word_len > col_num) 
+              while (word.length >= col_num)
+                  new_str += "<br/>#{word[0..(col_num - 2)]}-<br/>#{word[(col_num - 2)..-1]} "
+                  col_count = word[col_num..-1].length;
+                  word = word[col_num..-1]
+              end    
+          else 
+              new_str += "<br/>#{word} "
+              col_count = word_len
+          end
+      end
+      return new_str
+  end 
+end   
