4fbb254536fc1dfc18b416b62637d47a64b9aa32
atxrenegade <harleighabel@gmail.com>
2021-01-20 15:05:49 -0600

    fix ruby wrap method

--- app/models/wrapper.rb
+++ app/models/wrapper.rb
@@ -1,28 +1,29 @@
 class Wrapper 
-  #TO DO fix this algorithm
-  # condense head a tail to one block
+    def self.wrap(col_num, user_str)
+        word_arr = user_str.split(" ")
+        new_str = ""
+        col_idx = 0
 
-  def self.wrap(col_num, user_str) 
-      user_str_arr = user_str.split()
-      new_str = ""
-      col_count = 0
+        word_arr.each do |word|
+            if (col_idx > col_num || (col_idx + word.length) > col_num)
+                test_char = new_str.slice(-1)
+                if (test_char != "\n") then new_str.concat("\n") end
+                col_idx = 0
+            end
 
-      user_str_arr.each_with_index do |word, index|
-          word_len = word.length
-          if ((word_len + index) < col_num) 
-              new_str += "#{word}"
-              col_count += (wordLen + 1)
-          elsif (word_len > col_num) 
-              while (word.length >= col_num)
-                  new_str += "<br/>#{word[0..(col_num - 2)]}-<br/>#{word[(col_num - 2)..-1]} "
-                  col_count = word[col_num..-1].length;
-                  word = word[col_num..-1]
-              end    
-          else 
-              new_str += "<br/>#{word} "
-              col_count = word_len
-          end
-      end
-      return new_str
-  end 
+            if (word.length > col_num)
+                while (word.length > col_num) do
+                    new_str = new_str + "#{word[0...col_num - 1]}-\n"
+                    col_index = word[0...col_num - 1].length
+                    word = word.slice(col_num -1, word.length) 
+                end  
+            end   
+                
+            if (word.length <= col_num)
+                new_str += "#{word} "
+                col_idx += word.length + 1
+            end  
+        end  
+        puts(new_str)
+    end
 end   
